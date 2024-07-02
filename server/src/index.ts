62eda6d7e15ef7227c00a80452d0432abc03412f
ThomMarchand <thomasmarchand63@gmail.com>
2024-07-02 16:02:04 +0200

    workflows

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import mongoose from "mongoose";
-import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
 
 import { WilderResolver } from "./resolvers/wilder.resolver";
 
@@ -20,7 +19,6 @@ if (!process.env.SERVER_PORT) {
   });
   const server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginLandingPageDisabled()],
   });
 
   server.listen({ port: process.env.SERVER_PORT! }).then(({ url }) => {
